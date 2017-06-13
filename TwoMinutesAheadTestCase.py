77f0d871505120b3bbb18a610fdae23f1099f64a
HawiCaesar <brian.odhiambo932@gmail.com>
2017-06-13 18:23:01 +0300

    add TestCase to check if attack function works and added attack function

--- TwoMinutesAheadTestCase.py
+++ TwoMinutesAheadTestCase.py
@@ -1,21 +1,31 @@
 import unittest 
 
-from twominutesahead import Enemy, Troll, FlyingBat
+from twominutesahead import Enemy, Troll, FlyingBat, Player
 
 class TwoMinutesAheadTestCase(unittest.TestCase):
 
 	#Check if Troll and Flying bat are instances of Enemy
    	def test_troll_instance_of_enemy(self):
-   		troll = Troll('Fat-Belly-Troll', 'ROAAAAR', 1)
+   		troll = Troll('Fat-Belly-Troll', 'ROAAAAR', 1, 40)
    		self.assertIsInstance(troll, Enemy, msg='The object should be an instance of the `Enemy` class')
 
 
    	def test_flying_instance_of_enemy(self):
-   		bats = FlyingBat('Horn-Tailed-Bat', 'Screech-Screech-SCREECH', 35)
+   		bats = FlyingBat('Horn-Tailed-Bat', 'Screech-Screech-SCREECH', 35, 5)
    		self.assertIsInstance(bats, Enemy, msg='The object should be an instance of the `Enemy` class')
 
+   	# Check if zero enemies are brought to the game
    	def test_enemy_objects_is_zero(self):
-   		troll = Troll('Short-Snout-Troll', 'ROAAAAR', 0)
-		self.assertEqual(troll.attack(), "You are a lucky today they are not coming for you!", "No enemies for the moment")
+   		troll = Troll('Short-Snout-Troll', 'ROAAAAR', 0, 0)
+		self.assertEqual(troll.scare(), "You are a lucky today they are not coming for you!", "No enemies for the moment")
+
+	def test_player_is_in_game(self):
+		player1 = Player('sword')
+		self.assertEqual(player1.ready(), "I'm Ready, Bring It!", "Player has entered the game")
+
+	def test_enemy_can_attack(self):
+		manbat = FlyingBat('Swedish Bat', 'Screech-Screech-SCREECH', 32, 6)
+		self.assertEqual(manbat.attack(), "YOU BETTER THINK QUICKY AND GET OUT OF THERE!!!", "attack method should communicate to user")
+
 
 
--- twominutesahead.py
+++ twominutesahead.py
@@ -3,21 +3,38 @@ This is a very simple game of choices. You have '2 minute head start' on you ene
 You can only see 2 minutes ahead of them before they can attack you or you can block or make other decisions
 """
 
-#class player(object):
+class Player(object):
+	def __init__(self, weapon):
+		self.weapon = weapon
+		self.player_life = 100
+
+	
+	def ready(self):
+		return "I'm Ready, Bring It!"
+
+
+
 
 class Enemy(object):
 	 
-	 def __init__(self, enemy_type, sound, number_of_enemies):
+	 def __init__(self, enemy_type, sound, number_of_enemies, damage):
 	 	self.sound = sound
 	 	self.enemy_type = enemy_type
+	 	
+
 	 	try:
 	 		self.number_of_enemies = int(number_of_enemies)
 	 	except:
 	 		raise TypeError("Number of Enemies should be an integer whole number")
 
+	 	try:
+	 		self.damage = int(damage)
+
+	 	except:
+	 		raise TypeError("Damage of enemies is a whole number")
 
 
-	 def attack(self):
+	 def scare(self):
 	 	if self.number_of_enemies == 1:
 	 		print "There is %d %s behind you!\n %sing LOUDLY!!!" %(self.number_of_enemies, self.enemy_type, self.sound)
 
@@ -25,24 +42,41 @@ class Enemy(object):
 	 		print "There are %d %ss coming towards you!\n %sing LOUDLY!!!" %(self.number_of_enemies, self.enemy_type, self.sound)
 
 	 	else:
-	 		print "You are a lucky today they are not coming for you!"
+	 		return "You are a lucky today they are not coming for you!"
+
+	 def attack(self):
+	 	if self.number_of_enemies == 1:
+	 		print "The %s attacks you and you are in pain!!! -%d " %(self.enemy_type, self.damage)
+
+	 	elif self.number_of_enemies > 1:
+	 		print "The %ss attack you and you are in pain REAL PAIN!!! -%d of your life" %(self.enemy_type, self.damage)
+
+	 	return "YOU BETTER THINK QUICKY AND GET OUT OF THERE!!!"
+
 
 
 
 class Troll(Enemy):
-	def __init__(self, enemy_type, sound, number_of_enemies):
+	def __init__(self, enemy_type, sound, number_of_enemies, damage):
 		self.sound = sound
 	 	self.enemy_type = enemy_type
 	 	self.number_of_enemies = number_of_enemies
+	 	self.damage = damage
 
 		super(Enemy, self).__init__()
 
 
 class FlyingBat(Enemy):
-	def __init__(self, enemy_type, sound, number_of_enemies):
+	def __init__(self, enemy_type, sound, number_of_enemies, damage):
+		self.sound = sound
+	 	self.enemy_type = enemy_type
+	 	self.number_of_enemies = number_of_enemies
+	 	self.damage = damage
+
 		super(Enemy, self).__init__()
 
 
-troll = Troll('Fat-Belly-Troll', 'ROAAAAR', 1)
+troll = Troll('Fat-Belly-Troll', 'ROAAAAR', 1, 41)
 
-troll.attack()
+troll.scare()
+
